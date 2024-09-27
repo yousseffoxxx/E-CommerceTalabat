c419406d64db4bd23426561dda81475791806b80
Youssef Ahmed <youssef5fox5@gmail.com>
2024-09-27 23:35:47 +0300

    Part 10 Unit Of Work

---
+++ E-CommerceProject/Infrastructure/Presistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿global using Persistence.Data;
+global using System.Collections.Concurrent;
+
+namespace Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StoreContext _storeContext;
+        private readonly ConcurrentDictionary<string, object> _repositories;
+
+        public UnitOfWork(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+            _repositories = new();
+        }
+
+        public async Task<int> SaveChangesAsync() => await _storeContext.SaveChangesAsync();
+
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+            => (IGenericRepository<TEntity, TKey>)
+            _repositories.GetOrAdd(typeof(TEntity).Name, _ => new GenericRepository<TEntity, TKey>(_storeContext));
+            
+        //var typeName = typeof(TEntity).Name;
+            //if (_repositories.ContainsKey(typeName)) return (IGenericRepository<TEntity, TKey>)_repositories[typeName];
+            //var repo = new GenericRepository<TEntity, TKey>(_storeContext);
+            //_repositories.Add(typeName, repo);
+            //return repo;
+    }
+}
