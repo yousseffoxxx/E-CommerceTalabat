f73199fba9c3ce13c707d9885cee39712fd802e7
Youssef Ahmed <youssef5fox5@gmail.com>
2024-09-27 23:09:02 +0300

    Part 09 Generic Repositoriy

---
+++ E-CommerceProject/Infrastructure/Presistence/Repositories/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using Persistence.Data;
+
+namespace Persistence.Repositories
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreContext _storeContext;
+
+        public GenericRepository(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        public async Task AddAsync(TEntity entity) => await _storeContext.Set<TEntity>().AddAsync(entity);
+
+        public void Update(TEntity entity) => _storeContext.Set<TEntity>().Update(entity);    
+
+        public void Delete(TEntity entity) => _storeContext.Set<TEntity>().Remove(entity);
+
+        public async Task<TEntity?> GetAsync(TKey id) => await _storeContext.Set<TEntity>().FindAsync(id);    
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool trackChanges)
+           =>  trackChanges? await _storeContext.Set<TEntity>().ToListAsync()
+                : await _storeContext.Set<TEntity>().AsNoTracking().ToListAsync();
+            /*
+                if(trackChanges)
+                    return await _storeContext.Set<TEntity>().ToListAsync();
+                return await _storeContext.Set<TEntity>().AsNoTracking().ToListAsync();
+            */
+    }
+}
