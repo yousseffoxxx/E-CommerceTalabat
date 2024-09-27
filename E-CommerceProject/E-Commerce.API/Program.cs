9c20f384f0d10957d428d44a3546ec53df3dda00
Youssef Ahmed <youssef5fox5@gmail.com>
2024-09-27 22:46:36 +0300

    Part 08 DbInitializer

--- E-CommerceProject/E-Commerce.API/Program.cs
+++ E-CommerceProject/E-Commerce.API/Program.cs
@@ -1,28 +1,32 @@
 
+using Domain.Contracts;
 using Microsoft.EntityFrameworkCore;
+using Persistence;
 using Persistence.Data;
 
 namespace E_Commerce.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddScoped<IDbInitializer, DbInitializer>();
             builder.Services.AddDbContext<StoreContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
-            }
-            );
+            });
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+            await InitializeDbAsync(app);
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -39,6 +43,16 @@ namespace E_Commerce.API
             app.MapControllers();
 
             app.Run();
+
+            async Task InitializeDbAsync(WebApplication app)
+            {
+                // Create object from Type That Implements IDbInitializer
+                using var scope = app.Services.CreateScope();
+
+                var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+
+                await dbInitializer.InitializeAsync();
+            }
         }
     }
 }
