70c3a8b030bf9febb01c175f2f1ddc205b26ee8d
Youssef Ahmed <youssef5fox5@gmail.com>
2024-09-27 20:22:34 +0300

    Part 07 DbContext And Configuration

--- E-CommerceProject/Core/Domain/Entities/Product.cs
+++ E-CommerceProject/Core/Domain/Entities/Product.cs
@@ -11,7 +11,7 @@ namespace Domain.Entities
         public string Name { get; set; }
         public string Description { get; set; }
         public string PictureUrl { get; set; }
-        public decimal PriceTotal { get; set; }
+        public decimal Price { get; set; }
 
         public ProductBrand ProductBrand { get; set; } // Reference Navigational Prop
         public int BrandId { get; set; }
--- E-CommerceProject/E-Commerce.API/Program.cs
+++ E-CommerceProject/E-Commerce.API/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
 namespace E_Commerce.API
 {
     public class Program
@@ -10,6 +13,11 @@ namespace E_Commerce.API
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+            }
+            );
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
