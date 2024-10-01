4ccbd32c42cd7f7082b0fd972a48065fd5bcd1e1
Youssef Ahmed <youssef5fox5@gmail.com>
2024-10-01 20:29:28 +0300

    Part 05 Specifications

---
+++ E-CommerceProject/Core/Domain/Contracts/Specifications.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public abstract class Specifications<T> where T : class
+    {
+        protected Specifications(Expression<Func<T, bool>>? criteria)
+        {
+            Criteria = criteria;
+        }
+
+        public Expression<Func<T, bool>>? Criteria { get; }
+        public List<Expression<Func<T, object>>>? IncludeExpressions { get; } = new();
+        protected void AddInclude(Expression<Func<T, object>> expression)
+            => IncludeExpressions.Add(expression);
+
+
+    }
+}
---
+++ E-CommerceProject/Core/Services/Specifications/ProductWithBrandAndTypeSpecifications.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Contracts;
+using Domain.Entities;
+
+namespace Services.Specifications
+{
+    public class ProductWithBrandAndTypeSpecifications : Specifications<Product>
+    {
+        // Use to retrieve Id
+        public ProductWithBrandAndTypeSpecifications(int id) 
+            : base(product => product.id == id)
+        {
+            AddInclude(Product => Product.ProductBrand);
+            AddInclude(product => product.ProductType);
+        }
+
+        // use to get All Products
+        public ProductWithBrandAndTypeSpecifications() : base(null)
+        {
+            AddInclude(Product => Product.ProductBrand);
+            AddInclude(product => product.ProductType);
+
+        }
+    }
+}
