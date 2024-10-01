bb2e9c6c0506029fcbba5cf8f915da48b4b9d32c
Youssef Ahmed <youssef5fox5@gmail.com>
2024-10-01 20:42:35 +0300

    Part 06 Specifications Evaluator

---
+++ E-CommerceProject/Infrastructure/Presistence/Repositories/SpecificationEvaluator.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Contracts;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    internal static class SpecificationEvaluator
+    {
+        public static IQueryable<T> GetQuery<T>
+            (IQueryable<T> inputQuery , Specifications<T> specifications)
+            where T: class
+        {
+            var query = inputQuery;
+
+            if(specifications.Criteria is not null)
+                query = query.Where (specifications.Criteria);
+
+            query = specifications.IncludeExpressions.Aggregate(query, (currentQuery, includeExpression) => currentQuery
+                    .Include(includeExpression));
+
+            return query;
+        }
+    }
+}
