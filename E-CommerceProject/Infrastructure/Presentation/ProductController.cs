6060a7c959286c81017d63f98c212ba6cdcc24c6
Youssef Ahmed <youssef5fox5@gmail.com>
2024-10-15 19:10:47 +0300

    part 08 Add Authorization to Swagger Doc

--- E-CommerceProject/Infrastructure/Presentation/ProductController.cs
+++ E-CommerceProject/Infrastructure/Presentation/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Services.Abstractions;
 using Shared;
 using Shared.ErrorModels;
@@ -7,7 +8,7 @@ namespace Presentation
 {
     [ApiController]
     [Route("api/[controller]")]
-
+    [Authorize]
     public class ProductsController(IServiceManager ServiceManager) : ApiController
     {
         [HttpGet]
